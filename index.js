312b3e4d2c33b4191e57bea85ff5329c1ac55117
Ryan Sweeney <rasweeney94@yahoo.com>
2019-01-10 20:15:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function happyHolidayTo(holiday, name) {
 function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
 }
+
+function happyHolidaysTo (name) {
+  `Happy holidays, ${name}!`;
+}
